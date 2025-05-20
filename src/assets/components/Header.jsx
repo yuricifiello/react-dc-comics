b12f0c1ba53b0f16d0facb0400386c2f598ab34e
yuricifiello <yuricifiello@gmail.com>
2025-05-20 15:56:52 +0200

    first commit

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <Header>
+      <div className="container">
+        <img src="/assets/img/dc-logo.png" alt="Logo DC" />
+      </div>
+    </Header>
+  );
+}
