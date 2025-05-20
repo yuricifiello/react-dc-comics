292def4af103915328fff0178bb08153394a75e6
yuricifiello <yuricifiello@gmail.com>
2025-05-20 18:05:56 +0200

    footer

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,25 @@
+import footerbg from "/src/assets/img/footer-bg.jpg";
+import footerlogo from "/src/assets/img/dc-logo-bg.png";
+
+export default function Footer() {
+  return (
+    <footer>
+      <section id="main-footer">
+        <div className="container">
+          <ul>
+            DC COMICS
+            <li>
+              <a href="#">CHARACTER</a>
+              <a href="#">COMICS</a>
+              <a href="#">MOVIES</a>
+              <a href="#">TV</a>
+              <a href="#">GAMES</a>
+              <a href="#">VIDEOS</a>
+              <a href="#">NEWS</a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </footer>
+  );
+}
